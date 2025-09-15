9dab5ee0220967e6c7a368b4e1e8a2c10634b6ef
Korzh <Korolmedvedey@gmail.com>
2025-09-15 17:04:49 +0300

    Создал возможность пользователю вводить данные о покупках

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1.Вывод данных
+//    2.Статистика(среднее, максимальное, минимальное, сумма)
+//    3.Сортировка по цене(пузырьковая сортировка)
+//    4.Конвертация валюты(пользователь вводит курс или выбирает из списка)
+//    5.Поиск по названию 
+//    0. Выход
+
+using System.Net.Http.Headers;
+
+Console.WriteLine("Введите кол-во операций: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+string[] productOrService = new string[quantity];
+int[] cost = new int[quantity];
+for (int i = 0; i < quantity; i++)
+{
+    Console.WriteLine("Введите услугу или товар: ");
+    productOrService[i] = Console.ReadLine();
+    Console.WriteLine("Введите стоимость: ");
+    cost[i] = Convert.ToInt32(Console.ReadLine());
+    
+
+}
