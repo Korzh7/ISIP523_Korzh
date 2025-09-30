7c2c2772dc6f02edd23a086b03a5d79044e867b9
Korzh <Korolmedvedey@gmail.com>
2025-09-30 22:44:38 +0300

    Добавил подсчет букв и статистику по буквам

--- Program.cs
+++ Program.cs
@@ -112,4 +112,69 @@ class Program
             }
         }
 
-       
+        void LettersQuantity()
+        {
+            consonant = 0;
+            vowel = 0;
+
+            string allConsonant = "цкнгшщзхъфвпрлджчсмтьбйЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБЙ";
+            string allVowel = "уеыаоэяиюУЕЫАОЭЯИЮ";
+
+            foreach (char ch in text)
+            {
+                if (char.IsLetter(ch))
+                {
+                    bool isConsonant = false;
+                    for (int i = 0; i < allConsonant.Length; i++)
+                    {
+                        if (allConsonant[i] == ch)
+                        {
+                            isConsonant = true;
+                            break;
+                        }
+                    }
+
+                    if (isConsonant)
+                    {
+                        consonant++;
+                    }
+                    else
+                    {
+                        bool isVowel = false;
+                        for (int i = 0; i < allVowel.Length; i++)
+                        {
+                            if (allVowel[i] == ch)
+                            {
+                                isVowel = true;
+                                break;
+                            }
+                        }
+
+                        if (isVowel)
+                        {
+                            vowel++;
+                        }
+                    }
+                }
+            }
+        }
+
+        void StatsLetters()
+        {
+            azbyka.Clear();
+            foreach (char ch in text)
+            {
+                if (!char.IsLetter(ch))
+                    continue;
+
+                char lowerChar = char.ToLower(ch);
+
+                if (azbyka.ContainsKey(lowerChar))
+                    azbyka[lowerChar]++;
+                else
+                    azbyka[lowerChar] = 1;
+            }
+        }
+
+        
+        
