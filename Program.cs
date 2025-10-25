dee694067b5bae40aaccf8d0a61575bab738c08b
Korzh <Korolmedvedey@gmail.com>
2025-10-26 00:20:41 +0300

    Инициализировал основные классы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+class Abstract
+{
+    public int HP;
+    public int Defense;
+    public int Damage;
+
+}
+
+class Hero: Abstract
+{
+    Armor armor;
+    Weapon weapon;
+}
+
+class Enemy : Abstract
+{
+    List<string> Type = new List<string>();
+    public Enemy(int hp, int defense, int damage, List<string> type)
+    {
+        HP = hp;
+        Defense = defense;
+        Damage = damage;
+        Type = type;
+    }
+}
+
+class Armor
+{
+    public int Durability;
+    public decimal ArmorDefense;
+    public Armor(int durability, decimal armorDefense)
+    {
+        Durability = durability;
+        ArmorDefense = armorDefense;
+    }
+}
+
+class Weapon 
+{
+    public int Durability;
+    public int Damage;
+    public Weapon(int durability, int damage)
+    {
+        Durability = durability;
+        Damage = damage;
+    }
+
+}
+
+
+
