2444f305541698cb90dd47be4c64d3d56daab09b
Korzh <Korolmedvedey@gmail.com>
2025-11-01 20:43:03 +0300

    Базовая структура и главный цикл

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ISIP523_Korzh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeGame();
+
+            while (true)
+            {
+                Console.Clear();
+                DisplayGameStatus();
+
+                var clientInfo = GenerateRandomClient();
+                DisplayClientRequest(clientInfo);
+
+                DisplayActionMenu();
+                var choice = GetUserChoice();
+
+                switch (choice)
+                {
+                    case 1: AcceptOrder(clientInfo); break;
+                    case 2: DeclineOrder(clientInfo); break;
+                    case 3: ShowPurchaseMenu(); break;
+                    case 4: ShowWarehouseStatus(); break;
+                    case 5: ShowStatistics(); break;
+                    case 6: return;
+                    default: Console.WriteLine("Неверный выбор!"); break;
+                }
+
+                ProcessDeliveries();
+                CheckGameOver();
+
+                Console.WriteLine("\nНажмите любую клавишу...");
+                Console.ReadKey();
+            }
+        }
